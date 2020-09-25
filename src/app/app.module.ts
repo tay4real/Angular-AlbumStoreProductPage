b130b6c5b8a333a875641a0dc8a01c60cb7225e7
Ademuyiwa Otubusin <tay4real@gmail.com>
2020-09-25 23:19:11 +0100

    Completed module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,9 @@ import { ProductTracklistingComponent } from "./product-tracklisting/product-tra
 import { ProductListComponent } from "./product-list/product-list.component";
 
 const appRoutes: Routes = [
-  {
-    path: "products",
-    component: ProductListComponent,
-  },
+  { path: "products", component: ProductListComponent },
   { path: "product/:id", component: ProductPageComponent },
-  { path: "", redirectTo: "products", pathMatch: "full" },
+  { path: "", redirectTo: "/products", pathMatch: "full" },
 ];
 
 @NgModule({
